d1bd7a1b41c31f98bc76796bea18702d5a131e16
Gokul Kanish <66904387+imgokulkanish@users.noreply.github.com>
2021-09-08 18:59:49 +0530

    Pronote

---
+++ pronote/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Editor from './Component/Editor';
+import './Component/Editor.css'
+
+function App() {
+  return (
+    <div className="App">
+    <Editor/>
+    </div>
+  );
+}
+
+export default App;
